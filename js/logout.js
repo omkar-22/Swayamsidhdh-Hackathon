fdea9ab336925e4c50653bafa2cc36deaed65e7c
kmangesh49 <kumarmangesh55@gmail.com>
2020-08-13 16:55:41 +0530

    make pages public except theme registration form

--- js/logout.js
+++ js/logout.js
@@ -1,6 +1,19 @@
 import { firebase, auth } from './firebase'
 
 const logout = document.querySelector('#logout')
+
+function displayLogout() {
+	auth.onAuthStateChanged(function (user) {
+		if (user) {
+			logout.removeAttribute('hidden', '')
+		} else {
+			logout.querySelector('body').setAttribute('hidden', true)
+		}
+	})
+}
+
+displayLogout()
+
 logout.onclick = function () {
 	auth
 		.signOut()
