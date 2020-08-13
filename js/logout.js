9d3083877555089932077fc90693681ba55572b2
kmangesh49 <kumarmangesh55@gmail.com>
2020-08-13 17:14:35 +0530

    reload after logout

--- js/logout.js
+++ js/logout.js
@@ -2,23 +2,20 @@ import { firebase, auth } from './firebase'
 
 const logout = document.querySelector('#logout')
 
-function displayLogout() {
-	auth.onAuthStateChanged(function (user) {
-		if (user) {
-			logout.removeAttribute('hidden', '')
-		} else {
-			logout.querySelector('body').setAttribute('hidden', true)
-		}
-	})
-}
-
-displayLogout()
+auth.onAuthStateChanged(function (user) {
+	if (user) {
+		logout.removeAttribute('hidden', '')
+	} else {
+		logout.querySelector('body').setAttribute('hidden', true)
+	}
+})
 
 logout.onclick = function () {
 	auth
 		.signOut()
 		.then(function () {
 			// Sign-out successful.
+			window.location.replace(`${location.origin}/index.html`)
 		})
 		.catch(function (error) {
 			alert(error.message)
